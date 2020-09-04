7a4c0fd52218aef262fc80e9c59a4b9eaf60187d
Daryl Chen (Daz) <64760754+Daryl-creator@users.noreply.github.com>
2020-09-04 13:27:59 +1200

    add styling to cat component

--- client/components/CatFacts.jsx
+++ client/components/CatFacts.jsx
@@ -20,8 +20,8 @@ export default class CatFacts extends React.Component {
       <>
       <div class="column is-narrow has-text-centered">
         <div class="container is-fluid">
-        <h2 class="subtitle is-1 has-background-light">Heres your Cat Fact!</h2>
-        <p class="subtitle is-2 has-text-primary-dark has-background-light">{this.state.kitty}</p>
+        <h2 class="subtitle is-1 has-background-danger-light">Heres your Cat Fact!</h2>
+        <p class="subtitle is-2 has-text-primary-dark has-background-danger-light">{this.state.kitty}</p>
         <button class="button is-dark is-primary"><h3>Return to Animals</h3></button>
         </div>
         </div>
